38f621d045b988e86b80584d0f6a89f9fff21c4f
hi-rustin <rustin.liu@gmail.com>
2023-11-30 23:01:53 +0800

    chore: add linter and formatter
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['@nuxtjs/eslint-config-typescript', "prettier"],
+    rules: {
+        // Disables eslint throwing an error on script setup vue files
+        'import/first': 'off',
+    }
+};
--- app.vue
+++ app.vue
@@ -3,3 +3,23 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script>
+import { createPromiseClient } from "@connectrpc/connect";
+import { createGrpcWebTransport } from "@connectrpc/connect-web";
+import { Instrument } from "./gen/instrument_connect";
+import { InstrumentRequest } from "./gen/instrument_pb";
+
+const transport = createGrpcWebTransport({
+  baseUrl: "http://127.0.0.1:9091",
+  useBinaryFormat: true,
+});
+
+const client = createPromiseClient(Instrument, transport);
+const stream = client.watchUpdates(new InstrumentRequest());
+(async () => {
+  for await (const value of stream) {
+    console.log(value);
+  }
+})();
+</script>
