6a848d06ed416e1c9328ff629b0d68fb4b5f8df2
hi-rustin <rustin.liu@gmail.com>
2023-12-10 10:53:15 +0800

    fix: better format to duration
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- composables/duration.ts
+++ composables/duration.ts
@@ -24,42 +24,63 @@ export class Duration {
     }
 
     asMilliseconds(): number {
-        return Number(this.seconds) * 1000;
+        return Number(this.seconds) * 1000 + this.nanos / 1e6;
     }
 
     asMicroseconds(): number {
-        return Number(this.seconds) * 1000000 + this.nanos / 1000;
+        return Number(this.seconds) * 1e6 + this.nanos / 1000;
     }
 
     subtract(other: Duration): Duration {
-        return new Duration(
+        const result = new Duration(
             this.seconds - other.seconds,
             this.nanos - other.nanos,
         );
+        return result;
     }
 
     add(other: Duration): Duration {
-        return new Duration(
+        const result = new Duration(
             this.seconds + other.seconds,
             this.nanos + other.nanos,
         );
+        return result;
     }
 
     toString(): string {
-        if (this.asDays() >= 1) {
-            return `${Math.floor(this.asDays())}d`;
-        } else if (this.asHours() >= 1) {
-            return `${Math.floor(this.asHours())}h`;
-        } else if (this.asMinutes() >= 1) {
-            return `${Math.floor(this.asMinutes())}m`;
-        } else if (this.asSeconds() >= 1) {
-            return `${Math.floor(this.asSeconds())}s`;
-        } else if (this.asMilliseconds() >= 1) {
-            return `${Math.floor(this.asMilliseconds())}ms`;
-        } else if (this.asMicroseconds() >= 1) {
-            return `${Math.floor(this.asMicroseconds())}µs`;
+        const totalSeconds = this.asSeconds();
+        const units = [
+            { value: Math.floor(this.asDays()), unit: "d" },
+            { value: Math.floor(this.asHours()) % 24, unit: "h" },
+            { value: Math.floor(this.asMinutes()) % 60, unit: "m" },
+            { value: Math.floor(totalSeconds) % 60, unit: "s" },
+            { value: Math.floor(this.asMilliseconds()) % 1000, unit: "ms" },
+            { value: Math.floor(this.asMicroseconds()) % 1000, unit: "µs" },
+            { value: this.nanos, unit: "ns" },
+        ];
+
+        let str = "";
+        let partsIncluded = 0;
+
+        if (totalSeconds >= 60) {
+            // For durations longer than 60 seconds, include up to two non-zero units
+            for (const { value, unit } of units) {
+                if (value > 0) {
+                    str += `${value}${unit} `;
+                    partsIncluded++;
+                    if (partsIncluded === 2) break;
+                }
+            }
         } else {
-            return `${this.nanos}ns`;
+            // For durations shorter than 60 seconds, display the most significant non-zero unit
+            for (const { value, unit } of units) {
+                if (value > 0) {
+                    str = `${value}${unit}`;
+                    break;
+                }
+            }
         }
+
+        return str || `${this.nanos}ns`;
     }
 }
