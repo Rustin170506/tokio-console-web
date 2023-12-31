9a47daacfb5e5c688f226c922c2734b427a16716
hi-rustin <rustin.liu@gmail.com>
2023-12-31 21:10:10 +0800

    fix: do not add padding to logo and header
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- components/TokioLogo.vue
+++ components/TokioLogo.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex items-center bg-white dark:bg-gray-800 p-4 rounded-lg">
-        <TokioSvg/>
+    <div class="flex items-center bg-white dark:bg-gray-800 rounded-lg">
+        <TokioSvg />
         <h1 class="text-xl font-bold text-gray-900 dark:text-gray-100">
             Tokio Console
         </h1>
