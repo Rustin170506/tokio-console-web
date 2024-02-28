057be0f26ecf43387a6d12706200321096fcf660
hi-rustin <rustin.liu@gmail.com>
2024-02-28 20:03:46 +0800

    feat: force anchor on the top
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- utils/index.ts
+++ utils/index.ts
@@ -1,43 +1,4 @@
-import type { Ref } from "vue";
-import { getCurrentScope, onScopeDispose, ref, watch } from "vue";
-
-export function useObjectStorage<T>(
-    key: string,
-    initial: T,
-    listenToStorage = true,
-): Ref<T> {
-    const raw = localStorage.getItem(key);
-    const data = ref(raw ? JSON.parse(raw) : initial);
-
-    for (const key in initial) {
-        if (data.value[key] === undefined) data.value[key] = initial[key];
-    }
-
-    let updating = false;
-    let wrote = "";
-
-    watch(
-        data,
-        (value) => {
-            if (updating) return;
-            wrote = JSON.stringify(value);
-            localStorage.setItem(key, wrote);
-        },
-        { deep: true, flush: "post" },
-    );
-
-    if (listenToStorage) {
-        useEventListener(window, "storage", (e: StorageEvent) => {
-            if (e.key === key && e.newValue && e.newValue !== wrote) {
-                updating = true;
-                data.value = JSON.parse(e.newValue);
-                updating = false;
-            }
-        });
-    }
-
-    return data;
-}
+import { getCurrentScope, onScopeDispose, ref } from "vue";
 
 export function useEventListener(
     target: EventTarget,
