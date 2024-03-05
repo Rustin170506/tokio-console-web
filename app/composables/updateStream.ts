b9e6f5dcafa19754736521c8c85ff64124317d44
hi-rustin <rustin.liu@gmail.com>
2024-03-05 21:22:49 +0800

    feat: add backoff to update stream
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- app/composables/updateStream.ts
+++ app/composables/updateStream.ts
@@ -1,4 +1,5 @@
 import { ConnectError } from "@connectrpc/connect";
+import { backOff } from "exponential-backoff";
 import { state } from "./state";
 import { InstrumentRequest, Update } from "~/gen/instrument_pb";
 import { Timestamp } from "~/types/common/duration";
@@ -9,8 +10,11 @@ export function handleConnectError(err: any) {
 
     if (err instanceof ConnectError) {
         toast.add({
+            icon: "i-heroicons-x-circle",
             title: err.name,
             description: err.rawMessage,
+            // Don't close the toast automatically.
+            timeout: 0,
             color: "red",
         });
     }
@@ -44,23 +48,38 @@ export async function watchForUpdates(pending: Ref<boolean>) {
     }
 
     try {
-        const client = await useGrpcClient();
-        const updateStream = client.watchUpdates(new InstrumentRequest());
+        await backOff(
+            async () => {
+                const client = await useGrpcClient();
+                const updateStream = client.watchUpdates(
+                    new InstrumentRequest(),
+                );
 
-        for await (const value of updateStream) {
-            if (pending.value) {
-                pending.value = false;
-                state.isUpdateWatched = true;
-            }
-            updateLastUpdatedAt(value);
-            addMetadata(value);
-            addTasks(value);
-            retainTasks(state.retainFor);
-            addResources(value);
-            retainResources(state.retainFor);
-            addAsyncOps(value);
-            retainAsyncOps(state.retainFor);
-        }
+                for await (const value of updateStream) {
+                    if (pending.value) {
+                        pending.value = false;
+                        state.isUpdateWatched = true;
+                    }
+                    updateLastUpdatedAt(value);
+                    addMetadata(value);
+                    addTasks(value);
+                    retainTasks(state.retainFor);
+                    addResources(value);
+                    retainResources(state.retainFor);
+                    addAsyncOps(value);
+                    retainAsyncOps(state.retainFor);
+                }
+            },
+            {
+                retry: () => {
+                    // Set pending to true to display a loading indicator until reconnection.
+                    if (!pending.value) {
+                        pending.value = true;
+                    }
+                    return true;
+                },
+            },
+        );
     } catch (err) {
         handleConnectError(err);
     } finally {
