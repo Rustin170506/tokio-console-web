d85317c664331942bb627e7834acf09de74fe664
二手掉包工程师 <rustin.liu@gmail.com>
2024-03-20 23:58:58 +0800

    chore: add comments to histogram (#188)
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- app/histogram/src/lib.rs
+++ app/histogram/src/lib.rs
@@ -6,6 +6,21 @@ use hdrhistogram::Histogram;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+// Only used for debugging.
+#[allow(unused_macros)]
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+/// Contains the serialized histogram and the min and max values.
+#[wasm_bindgen]
 pub struct MiniHistogram {
     percentiles: Vec<Percentile>,
     data: Vec<DurationCount>,
@@ -13,6 +28,7 @@ pub struct MiniHistogram {
     pub max: u64,
 }
 
+// Expose the MiniHistogram to JavaScript.
 #[wasm_bindgen]
 impl MiniHistogram {
     #[wasm_bindgen(getter)]
@@ -26,6 +42,7 @@ impl MiniHistogram {
     }
 }
 
+/// Contains the duration and the count of that duration.
 #[wasm_bindgen]
 #[derive(Clone)]
 pub struct DurationCount {
@@ -33,6 +50,7 @@ pub struct DurationCount {
     pub count: u64,
 }
 
+// Expose the DurationCount to JavaScript.
 #[wasm_bindgen]
 impl DurationCount {
     #[wasm_bindgen(constructor)]
@@ -41,6 +59,7 @@ impl DurationCount {
     }
 }
 
+/// Contains the percentile and the value at that percentile.
 #[wasm_bindgen]
 #[derive(Clone)]
 pub struct Percentile {
@@ -48,6 +67,7 @@ pub struct Percentile {
     pub value: u64,
 }
 
+// Expose the Percentile to JavaScript.
 #[wasm_bindgen]
 impl Percentile {
     #[wasm_bindgen(constructor)]
@@ -56,14 +76,18 @@ impl Percentile {
     }
 }
 
+/// Deserializes the histogram from the given bytes.
 #[wasm_bindgen(js_name = deserializeHistogram)]
 pub fn deserialize_histogram(bytes: &[u8]) -> MiniHistogram {
     let histogram: Histogram<u64> = hdrhistogram::serialization::Deserializer::new()
         .deserialize(&mut Cursor::new(&bytes))
         .expect("Failed to deserialize histogram");
+
     let min = histogram.min();
     let max = histogram.max();
+    // TODO: make the step size configurable.
     let step_size = ((max - min) as f64 / 40 as f64).ceil() as u64 + 1;
+
     let mut found_first_nonzero = false;
     let data: Vec<DurationCount> = histogram
         .iter_linear(step_size)
@@ -76,16 +100,16 @@ pub fn deserialize_histogram(bytes: &[u8]) -> MiniHistogram {
                 None
             } else {
                 found_first_nonzero = true;
-                Some((value.value_iterated_to(), count))
+                Some(DurationCount::new(value.value_iterated_to(), count))
             }
         })
-        .map(|(duration, count)| DurationCount::new(duration, count))
         .collect();
+
     // Get the important percentile values from the histogram
-    let pairs = [10f64, 25f64, 50f64, 75f64, 90f64, 95f64, 99f64]
+    let percentiles = [10f64, 25f64, 50f64, 75f64, 90f64, 95f64, 99f64]
         .iter()
-        .map(move |i| (*i, histogram.value_at_percentile(*i)));
-    let percentiles = pairs.map(|(p, v)| Percentile::new(p, v)).collect();
+        .map(|&i| Percentile::new(i, histogram.value_at_percentile(i)))
+        .collect();
 
     MiniHistogram {
         data,
