aed13dcb34dfd08e2006166d7cca0e26cf1532ef
二手掉包工程师 <rustin.liu@gmail.com>
2024-03-20 22:36:11 +0800

    refactor: add more comments to the task details (#187)
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- app/types/task/tokioTaskDetails.ts
+++ app/types/task/tokioTaskDetails.ts
@@ -6,16 +6,27 @@ import {
 } from "~/histogram/pkg/histogram";
 import type { TaskDetails } from "~/gen/tasks_pb";
 
+/**
+ * Percentile represents a percentile value and its corresponding duration.
+ * For example, a 99th percentile of 100ms means that 99% of the data points are less than or equal to 100ms.
+ */
 export interface Percentile {
     percentile: number;
     duration: Duration;
 }
 
+/**
+ * DurationCount represents a duration and the number of times that duration was observed.
+ */
 export interface DurationCount {
     duration: Duration;
     count: bigint;
 }
 
+/**
+ * DurationDetails represents the details of a duration histogram.
+ * It includes the percentiles, histogram, max, and min values.
+ */
 export interface DurationDetails {
     percentiles: Percentile[];
     histogram: DurationCount[];
@@ -23,29 +34,36 @@ export interface DurationDetails {
     min: Duration;
 }
 
+/**
+ * TokioTaskDetails represents the details of a tokio task.
+ * It includes the poll times and scheduled times.
+ */
 export interface TokioTaskDetails {
     pollTimes: DurationDetails;
     scheduledTimes?: DurationDetails;
 }
 
+/**
+ * fromProtoTaskDetails converts a protobuf TaskDetails to a TokioTaskDetails.
+ * It includes the poll times and scheduled times histograms.
+ * @param details - the protobuf TaskDetails
+ * @returns the TokioTaskDetails
+ */
 export function fromProtoTaskDetails(details: TaskDetails): TokioTaskDetails {
-    const convertToDurationPercentile = (p: RawPercentile) => ({
+    const toDurationPercentile = (p: RawPercentile) => ({
         percentile: p.percentile,
         duration: Duration.fromNano(p.value),
     });
-
-    const convertToDurationCount = (p: RawDurationCount) => ({
+    const toDurationCount = (p: RawDurationCount) => ({
         duration: Duration.fromNano(p.duration),
         count: p.count,
     });
 
     const createHistogram = (rawHistogram: Uint8Array) => {
+        // wasm function.
         const miniHistogram = deserializeHistogram(rawHistogram);
-
-        const percentiles = miniHistogram.percentiles.map(
-            convertToDurationPercentile,
-        );
-        const histogram = miniHistogram.data.map(convertToDurationCount);
+        const percentiles = miniHistogram.percentiles.map(toDurationPercentile);
+        const histogram = miniHistogram.data.map(toDurationCount);
 
         return {
             percentiles,
@@ -55,27 +73,18 @@ export function fromProtoTaskDetails(details: TaskDetails): TokioTaskDetails {
         };
     };
 
-    let pollTimes: TokioTaskDetails["pollTimes"] | undefined;
-    let scheduledTimes: TokioTaskDetails["scheduledTimes"] | undefined;
+    const pollTimes =
+        details.pollTimesHistogram.case === "histogram"
+            ? createHistogram(details.pollTimesHistogram.value.rawHistogram)
+            : // legacyHistogram
+              createHistogram(details.pollTimesHistogram.value!);
 
-    if (details.pollTimesHistogram.case === "histogram") {
-        pollTimes = createHistogram(
-            details.pollTimesHistogram.value.rawHistogram,
-        );
-    }
-
-    if (details.pollTimesHistogram.case === "legacyHistogram") {
-        pollTimes = createHistogram(details.pollTimesHistogram.value);
-    }
-
-    if (details.scheduledTimesHistogram?.rawHistogram) {
-        scheduledTimes = createHistogram(
-            details.scheduledTimesHistogram.rawHistogram,
-        );
-    }
+    const scheduledTimes = details.scheduledTimesHistogram?.rawHistogram
+        ? createHistogram(details.scheduledTimesHistogram.rawHistogram)
+        : undefined;
 
     return {
-        pollTimes: pollTimes!,
+        pollTimes,
         scheduledTimes,
     };
 }
