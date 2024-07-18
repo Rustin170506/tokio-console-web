639fd85e8ffefc889cd288f7f6f75ac9417a3859
hi-rustin <rustin.liu@gmail.com>
2024-07-18 21:52:33 +0800

    feat: add some alerts
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- app/components/WarningsSlideover.vue
+++ app/components/WarningsSlideover.vue
@@ -1,7 +1,27 @@
 <template>
-    <USlideover :overlay="false">
-        <div class="p-4 flex-1">
-            <Placeholder class="h-full" />
+    <USlideover>
+        <div class="p-4 flex-1 space-y-4">
+            <UAlert
+                icon="i-heroicons-exclamation-triangle-20-solid"
+                color="yellow"
+                variant="outline"
+                title="Never Yield"
+                description="Tasks have never yielded (threshold 1000ms)"
+            />
+            <UAlert
+                icon="i-heroicons-exclamation-triangle-20-solid"
+                color="yellow"
+                variant="outline"
+                title="Lost Waker"
+                description="Tasks have lost their waker"
+            />
+            <UAlert
+                icon="i-heroicons-exclamation-triangle-20-solid"
+                color="yellow"
+                variant="outline"
+                title="Self Wake"
+                description="Tasks have woken themselves over 50% of the time"
+            />
         </div>
     </USlideover>
 </template>
