bcefbda6971a0dc7b2bec9faf7c5224bb0e87c23
hi-rustin <29879298+Rustin170506@users.noreply.github.com>
2024-09-01 00:46:04 +0800

    fix: print path
    Signed-off-by: hi-rustin <29879298+Rustin170506@users.noreply.github.com>

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
+use std::env;
 use std::process::{exit, Command};
 
 fn check_command_exists(command: &str) -> bool {
-    Command::new("which")
-        .arg(command)
+    Command::new(command)
+        .arg("--version")
         .output()
         .map(|output| output.status.success())
         .unwrap_or(false)
@@ -26,7 +27,15 @@ fn main() {
     let required_commands = ["node", "pnpm", "wasm-pack"];
     for &cmd in required_commands.iter() {
         if !check_command_exists(cmd) {
-            eprintln!("Error: {} is not installed. Please install it to continue.", cmd);
+            eprintln!(
+                "Error: {} is not found in PATH. Current PATH: {:?}",
+                cmd,
+                env::var("PATH")
+            );
+            eprintln!(
+                "Error: {} is not installed or not in PATH. Please install it to continue.",
+                cmd
+            );
             if cmd == "wasm-pack" {
                 eprintln!("You can install it using: cargo install wasm-pack");
             }
